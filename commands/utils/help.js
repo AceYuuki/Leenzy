9700f2a720703685ba40ff73d26ef0e87baa701c
Yuuki <yuukiskiiy@gmail.com>
2023-10-22 18:02:15 +0200

    heroku

--- commands/utils/help.js
+++ commands/utils/help.js
@@ -21,15 +21,14 @@ module.exports = {
         if (!args.length){
             const noArgsEmbed1 = new MessageEmbed()
             .setColor('BLUE')
-            .addField({ name: 'Liste des commandes', value: `Une liste de toutes les catégories disponibles et leurs commandes. \nPour plus d\'informations sur une commande, taper \`/help <command>\``})
-
-            for (const category of commandFolder) {
-                noArgsEmbed1.addField(
-                    `+ ${category.replace(/(^\w|\s\w)/g, firstLetter => firstLetter.toUpperCase())}`,
-                    `\`${client.commands.filter(cmd => cmd.category == category.toLowerCase()).map(cmd => cmd.name).join(', ')}\``
-                );
-            }
-
+            .addField('__Liste des commandes__', `Une liste de toutes les catégories disponible et leurs commands\nPour plus d'informations sur une commande , taper \`${prefix}help [commande]\`\n** **`)
+     
+            for(const category of commandFolder){
+                noArgsEmbed1.addField( 
+                    `|__${category.replace(/(^\w|\s\w)/g, firstLetter => firstLetter.toUpperCase())}:__`,
+                    `\`${client.commands.filter(cmd => cmd.category == category.toLowerCase()).map(cmd => cmd.name).join(', ')}\``, true
+                     );
+            };
             return message.channel.send({ embeds: [noArgsEmbed1] });
         };
         const cmd = client.commands.get(args[0]);
@@ -63,43 +62,48 @@ Ne pas inclure ces caractères -> {}, [] et <> dans vos commandes.
         required: false,
         }
     ],
-    async runInteraction(client, interaction, guildSettings) {
-        const prefix = guildSettings.prefix;
+    runInteraction(client, interaction, guildSettings){
         const cmdName = interaction.options.getString('command');
+        const prefix = guildSettings.prefix;
 
-        if (!cmdName) {
+        if (!cmdName){
             const noArgsEmbed = new MessageEmbed()
             .setColor('BLUE')
-            .addFields({ name: 'Liste des commandes', value: `Une liste de toutes les catégories disponibles et leurs commandes. \nPour plus d\'informations sur une commande, taper \`/help <command>\``},)
-
-            for (const category of commandFolder) {
-                noArgsEmbed.addField(
-                    `+ ${category.replace(/(^\w|\s\w)/g, firstLetter => firstLetter.toUpperCase())}`,
-                    `\`${client.commands.filter(cmd => cmd.category == category.toLowerCase()).map(cmd => cmd.name).join(', ')}\``
-                );
+            .addField('__Liste des commandes__', `Une liste de toutes les catégories disponible et leurs commands\nPour plus d'informations sur une commande , taper \`${prefix}help [commande]\`\n** **`)
+     
+            for(const category of commandFolder){
+                noArgsEmbed.addField( 
+                    `|__${category.replace(/(^\w|\s\w)/g, firstLetter => firstLetter.toUpperCase())}:__`,
+                    `\`${client.commands.filter(cmd => cmd.category == category.toLowerCase()).map(cmd => cmd.name).join(', ')}\``,true
+                     );
             }
-
             return interaction.reply({ embeds: [noArgsEmbed], ephemeral: true});
         }
-
         const cmd = client.commands.get(cmdName);
-        if (!cmd) return interaction.reply({ content: 'cette commande n\'existe pas ! ', ephemeral: true})
+        if (!cmd) return interaction.reply({content: `Cette commande n'existe pas !`, ephemeral: true });
 
-        return interaction.reply({ content: `
+        const argsEmbed = new MessageEmbed()
+        .setColor('BLUE')
+        .setTitle(`**__${cmd.name.replace(/(^\w|\s\w)/g, firstLetter => firstLetter.toUpperCase())}__**`)
+        .setDescription(`
+        
 \`\`\`makefile
-[Help: Commande => ${cmd.name}] ${cmd.ownerOnly ? '/!\\ Pour les admins du bot uniquements' : ''}
+[Help Commande -> ${cmd.name}] ${cmd.ownerOnly ? '/!\\ Pour les admins du bot uniquement /!\\' : ''}
 
 ${cmd.description ? cmd.description : contextDescription[`${cmd.name}`]}
 
-Utilisation : ${prefix}${cmd.usage}
-Exemples : ${prefix}${cmd.examples.join(` | ${prefix}`)}
-Permissions : ${cmd.permissions.join(', ')}
+Permissions: ${cmd.permissions.join(', ')}
+Utilisation: ${prefix}${cmd.usage}
+Exemples: ${prefix}${cmd.exemples.join(` | ${prefix}`)}
 
 ---
 
-${prefix} = prefix utiliser pour le bot (/ commands sont aussi disponibles)
-{} = sous -commande(s) disponible(s) | [] = option(s) obligatoire(s) | <> = option(s) optionnel(s)
+${prefix} = prefix utilisé pour le bot (/commands sont aussi disponibles)
+{} = sous-commande(s) disponible(s) | [] = option(s) obligatoire(s) | <> = option(s) facultatif(s)
 Ne pas inclure ces caractères -> {}, [] et <> dans vos commandes.
-\`\`\``, ephemeral: true})
+\`\`\`
+        
+        `)
+        return interaction.reply({ embeds: [argsEmbed], ephemeral: true});
     }
 }
